f66e8fd12e4cbb1925865ac956c050c0c6b68ed1
ritik-mewada <ritikmewada@gmail.com>
2023-05-18 17:33:02 +0530

    product controller done

--- controllers/productController.js
+++ controllers/productController.js
@@ -58,3 +58,84 @@ exports.getAllProducts = BigPromise(async (req, res, next) => {
         filteredProductNumber,
     });
 });
+
+exports.getOneProduct = BigPromise(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new CustomError("no product found with this id", 401));
+    }
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+});
+
+exports.adminGetAllProducts = BigPromise(async (req, res, next) => {
+    const products = await Product.find();
+
+    res.status(200).json({
+        success: true,
+        products,
+    });
+});
+
+exports.adminUpdateOneProduct = BigPromise(async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new CustomError("no product found with this id", 401));
+    }
+    let imagesArray = [];
+
+    if (req.files) {
+        for (let index = 0; index < product.photos.length; index++) {
+            const res = await cloudinary.uploader.destroy(
+                product.photos[index].id
+            );
+        }
+
+        for (let index = 0; index < req.files.photos.length; index++) {
+            const result = await cloudinary.uploader.upload(
+                req.files.photos[index].tempFilePath,
+                {
+                    folder: "products",
+                }
+            );
+            imagesArray.push({
+                id: result.public_id,
+                secure_url: result.secure_url,
+            });
+        }
+    }
+
+    req.body.photos = imagesArray;
+
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+
+    res.status(200).json({
+        success: true,
+        product,
+    });
+});
+
+exports.adminDeleteOneProduct = BigPromise(async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return next(new CustomError("no product found with this id", 401));
+    }
+
+    for (let index = 0; index < product.photos.length; index++) {
+        const res = await cloudinary.uploader.destroy(product.photos[index].id);
+    }
+
+    await product.deleteOne();
+
+    res.status(200).json({
+        success: true,
+        message: "product was deleted!!!",
+    });
+});
--- routes/product.js
+++ routes/product.js
@@ -4,13 +4,25 @@ const { isLoggedIn, customRole } = require("../middlewares/userMiddleware");
 const {
     addProduct,
     getAllProducts,
+    adminGetAllProducts,
+    getOneProduct,
+    adminUpdateOneProduct,
+    adminDeleteOneProduct,
 } = require("../controllers/productController");
 
 // user routes
 router.route("/products").get(getAllProducts);
+router.route("/product/:id").get(getOneProduct);
 
 // admin routes
 router
     .route("/admin/product/add")
     .post(isLoggedIn, customRole("admin"), addProduct);
+router
+    .route("/admin/products")
+    .get(isLoggedIn, customRole("admin"), adminGetAllProducts);
+router
+    .route("/admin/product/:id")
+    .put(isLoggedIn, customRole("admin"), adminUpdateOneProduct)
+    .delete(isLoggedIn, customRole("admin"), adminDeleteOneProduct);
 module.exports = router;
